8392846084daccc8e674e625165ba74389b67a9b
Madeline Redd <madeline.e.redd@gmail.com>
2025-02-13 16:01:50 -0600

    Class coding challenge two

---
+++ 2025_2_13_DataVisualization_ClassAssignment_mer0127.R
@@ -0,0 +1,40 @@
+##################################################################################################################
+#Author: Madeline Redd-Hamrick (Maddie) (mer0127)
+
+#git config --global user.name 'Mads-Hamrick'
+#git config --global user.email 'madeline.e.redd@gmail.com'
+
+#2025 February 13
+
+rm(list = ls(all=TRUE)) #Clears Environment to see if code can stand alone
+
+#Class:ENTM 6820
+#Assignment Name: Data Visualization Code Challenge Two
+
+###################################################################################################################
+#Import data from project
+
+Mycotoxin<- read.csv("MycotoxinData.csv")
+
+library(ggplot2)
+
+###################################################################################################################
+
+ggplot( 
+  
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
